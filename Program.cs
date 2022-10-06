dd3145abda89c4b49b3f252efdd3d725dc65470f
TRK856 <53193704+TRK856@users.noreply.github.com>
2022-10-06 09:55:48 -0600

    someomer work

--- Program.cs
+++ Program.cs
@@ -11,6 +11,19 @@ else
     Console.WriteLine("8 is NOT in the List");
 }
 
+// returns index
+int index = indexOfInt(nums, 8);
+if (index != -1) {
+  Console.WriteLine($"8 IN list at index: {index}");
+} else {
+  Console.WriteLine("8 NOT in list");
+}
+
+// returns reversed array
+List<int> reversedNums = reverse(nums);
+Console.WriteLine(reversedNums);
+
+
 bool intContains(List<int> anArray, int find)
 {
     for (int i = 0; i < anArray.Count(); i++)
@@ -33,4 +46,35 @@ bool stringContains(List<string> anArray, string find)
         }
     }
     return false;
+}
+
+int indexOfInt(List<int> anArray, int find) {
+    for (int i = 0; i < anArray.Count(); i++)
+    {
+        if (anArray[i] == find)
+        {
+            return i;
+        }
+    }
+    return -1;
+}
+
+int indexOfString(List<string> anArray, string find) {
+    for (int i = 0; i < anArray.Count(); i++)
+    {
+        if (anArray[i] == find)
+        {
+            return i;
+        }
+    }
+    return -1;
+}
+
+List<string> reverse(List<string> anArray) {
+    List<string> reverse;
+    for (int i = anArray.Count(); i >anArray.Count(); i--)
+    {
+        reverse.Add(anArray[i]);
+    }
+    return anArray;
 }
