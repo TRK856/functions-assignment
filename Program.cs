5e8405fa5271202eeb2d741dc7e11e08fa227f39
TRK856 <53193704+TRK856@users.noreply.github.com>
2022-10-07 09:50:12 -0600

    lol

--- Program.cs
+++ Program.cs
@@ -13,16 +13,30 @@ else
 
 // returns index
 int index = indexOfInt(nums, 8);
-if (index != -1) {
-  Console.WriteLine($"8 IN list at index: {index}");
-} else {
-  Console.WriteLine("8 NOT in list");
+if (index != -1)
+{
+    Console.WriteLine($"8 IN list at index: {index}");
+}
+else
+{
+    Console.WriteLine("8 NOT in list");
 }
 
 // returns reversed array
-List<int> reversedNums = reverse(nums);
-Console.WriteLine(reversedNums);
+writeList(reverseInt(nums));
+Console.WriteLine("---------\nswap\n--------");
+swapInt(nums, 2, 4);
+writeList(nums);
 
+// nums -> [2, 4, 10, 8, 6]
+
+void writeList(List<int> anArray)
+{
+    for (int i = 0; i < anArray.Count(); i++)
+    {
+        Console.WriteLine(anArray[i]);
+    }
+}
 
 bool intContains(List<int> anArray, int find)
 {
@@ -36,19 +50,32 @@ bool intContains(List<int> anArray, int find)
     return false;
 }
 
-bool stringContains(List<string> anArray, string find)
+// bool stringContains(List<string> anArray, string find)
+// {
+//     for (int i = 0; i < anArray.Count(); i++)
+//     {
+//         if (anArray[i] == find)
+//         {
+//             return true;
+//         }
+//     }
+//     return false;
+// }
+
+int indexOfInt(List<int> anArray, int find)
 {
     for (int i = 0; i < anArray.Count(); i++)
     {
         if (anArray[i] == find)
         {
-            return true;
+            return i;
         }
     }
-    return false;
+    return -1;
 }
 
-int indexOfInt(List<int> anArray, int find) {
+int indexOfString(List<string> anArray, string find)
+{
     for (int i = 0; i < anArray.Count(); i++)
     {
         if (anArray[i] == find)
@@ -59,22 +86,40 @@ int indexOfInt(List<int> anArray, int find) {
     return -1;
 }
 
-int indexOfString(List<string> anArray, string find) {
-    for (int i = 0; i < anArray.Count(); i++)
-    {
-        if (anArray[i] == find)
-        {
-            return i;
-        }
-    }
-    return -1;
-}
-
-List<string> reverse(List<string> anArray) {
-    List<string> reverse;
-    for (int i = anArray.Count(); i >anArray.Count(); i--)
+List<int> reverseInt(List<int> anArray)
+{
+    List<int> reverse = new List<int> { };
+    int count = 0;
+    for (int i = anArray.Count() - 1; i >= 0; i--)
     {
         reverse.Add(anArray[i]);
+        count++;
     }
-    return anArray;
+    return reverse;
 }
+
+// List<string> reverseString(List<string> anArray) {
+//     List<string> reverse = new List<string> {};
+//     int count = 0;
+//     for (int i = anArray.Count(); i >anArray.Count(); i--)
+//     {
+//         count++;
+//         reverse[count] = anArray[i];
+//     }
+//     return reverse;
+// }
+
+void swapInt(List<int> anArray, int index1, int index2)
+{
+    int store = anArray[index1];
+    anArray[index1] = anArray[index2];
+    anArray[index2] = store;
+}
+
+// void swapString(List<string> anArray, int index1, int index2)
+// {
+//     string store = anArray[index1];
+//     anArray[index1] = anArray[index2];
+//     anArray[index2] = store;
+// }
+
